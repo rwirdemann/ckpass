242e2362da6198fcd22fd6ab8a4ffd1f8031b4ee
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2020-08-13 14:56:44 +0200

    Cleans up pom

--- src/main/java/de/codekeepers/CKPass.java
+++ src/main/java/de/codekeepers/CKPass.java
@@ -1,23 +1,29 @@
 package de.codekeepers;
 
-import de.brendamour.jpasskit.PKBarcode;
-import de.brendamour.jpasskit.PKField;
-import de.brendamour.jpasskit.PKLocation;
-import de.brendamour.jpasskit.PKPass;
-import de.brendamour.jpasskit.enums.PKBarcodeFormat;
-import de.brendamour.jpasskit.passes.PKGenericPass;
-import de.brendamour.jpasskit.signing.*;
-import org.apache.commons.io.IOUtils;
+import de.brendamour.jpasskit.signing.PKSigningInformation;
+import de.brendamour.jpasskit.signing.PKSigningInformationUtil;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.security.cert.CertificateException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class CKPass {
+    private static final String keyStorePath = "certs/pass.p12";
+    private static final String keyStorePassword = "start1234";
+    private static final String appleWWDRCA = "certs/AppleWWDRCA.cer";
+
+    private PKSigningInformation pkSigningInformation;
+
+    public CKPass() {
+        try {
+            pkSigningInformation = new PKSigningInformationUtil().loadSigningInformationFromPKCS12AndIntermediateCertificate(keyStorePath, keyStorePassword, appleWWDRCA);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
 
---
+++ src/test/java/CKPassTest.java
@@ -0,0 +1,10 @@
+import de.codekeepers.CKPass;
+import org.junit.Test;
+
+public class CKPassTest {
+
+    @Test
+    public void HappyPath() {
+        CKPass pass = new CKPass();
+    }
+}
